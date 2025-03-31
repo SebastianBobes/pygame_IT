aa5fe90dbc9d57bbd35e282a65cf7f71bb036eea
Petru1803 <petrudmarina@gmail.com>
2025-03-31 21:17:01 +0300

    .

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
 import pygame
 import random
 import os
+import psycopg2
+
+def get_player_name():
+    try:
+        with open("player.txt", "r") as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        print("Error: name.txt not found. Using default name.")
+        return "UnknownPlayer"
+    
+def get_player_score():
+    try:
+        with open("score.txt", "r") as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        print("Error: score.txt not found. Using default score.")
+        return 0 
+
+DATABASE_URL = "adresa_baza_date" 
+
+def connect_db():
+    """Connect to PostgreSQL database."""
+    try:
+        return psycopg2.connect(DATABASE_URL, sslmode="require")
+    except Exception as e:
+        print("Database Connection Error:", e)
+        return None
+
+def save_score_db(player_name, score):
+    """Save player score to the database."""
+    conn = connect_db()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO joc (nume, scor) VALUES (%s, %s)", (player_name, score))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            print(f"Score saved: {player_name} - {score}")
+        except Exception as e:
+            print("Error saving score:", e)
+
 
 # Initializare
 pygame.init()
@@ -46,6 +88,9 @@ def save_score(score):
         file.write(str(total_score))
     print(f"Score salvat: {total_score}")  #  Pentru debug
 
+player_name= get_player_name()
+score= get_player_score()
+
 # Atribute pentru caracter
 player_x, player_y = 0, 0 
 steps = 0
@@ -55,7 +100,7 @@ bot_x, bot_y = 9, 9
 
 # Obstacol
 obstacles = set()
-while len(obstacles) < 15:  # Numar obstacole
+while len(obstacles) < 20:  # Numar obstacole
     ox, oy = random.randint(0, COLS - 1), random.randint(0, ROWS - 1)
     if (ox, oy) not in {(player_x, player_y), (bot_x, bot_y)}:  # Evita spawn-ul pe player/bot
         obstacles.add((ox, oy))
@@ -81,6 +126,9 @@ def bot_move(bot_x, bot_y, player_x, player_y, obstacles):
 running = True
 walking = True
 game_over = False  
+won = False
+over=False
+k=1
 
 while running:
     screen.fill(BLACK)  
@@ -93,15 +141,15 @@ while running:
                 pygame.draw.rect(screen, BLUE, rect)  # Desenam obstacole
             pygame.draw.rect(screen, GRAY, rect, 1)  # Desenam conturul fiecarei casute
     
-    player_image = pygame.image.load("Capture.jpg") 
-    player_image = pygame.transform.scale(player_image, (CELL_SIZE, CELL_SIZE))
     # Afisare jucator
-    screen.blit(player_image, (player_x * CELL_SIZE, player_y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+    pygame.draw.rect(screen, WHITE, 
+        (player_x * CELL_SIZE, player_y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+    )
     
-    bot_image = pygame.image.load("police.jpg")
-    bot_image = pygame.transform.scale(bot_image, (CELL_SIZE, CELL_SIZE))
     # Afisare bot
-    screen.blit(bot_image, (bot_x * CELL_SIZE, bot_y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+    pygame.draw.rect(screen, RED, 
+        (bot_x * CELL_SIZE, bot_y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+    )
     
     # Afisare numar pasi
     counter_text = font.render(f"Steps: {steps}", True, WHITE)
@@ -117,7 +165,7 @@ while running:
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
 
-    if walking and not game_over:
+    if walking and not game_over and not won:
         keys = pygame.key.get_pressed()
         new_x, new_y = player_x, player_y
         
@@ -139,12 +187,33 @@ while running:
             game_over = True  
             walking = False 
             save_score(steps)  # Salvează scorul înainte de a termina jocul
+        if (player_x, player_y) == (9,9):
+            won = True  
+            walking = False 
+            save_score(steps)  # Salvează scorul înainte de a termina jocul
             
 
     # Afisare Game Over
+    if game_over and over==False and k==5:
+        game_over_text = font.render("Game Over", True, WHITE)
+        screen.blit(game_over_text, (WIDTH // 2 - 80, HEIGHT // 2))
+        save_score_db(player_name, score)
+        over=True
+    
     if game_over:
         game_over_text = font.render("Game Over", True, WHITE)
         screen.blit(game_over_text, (WIDTH // 2 - 80, HEIGHT // 2))
+   
+    #Afisare castig
+    if won and over==False and k==5:
+        won_text = font.render("You won!", True, WHITE)
+        screen.blit(won_text, (WIDTH // 2 - 80, HEIGHT // 2))
+        save_score_db(player_name, score)
+        over=True
+    
+    if won:
+        won_text = font.render("You won!", True, WHITE)
+        screen.blit(won_text, (WIDTH // 2 - 80, HEIGHT // 2))
     
     pygame.display.flip()
     pygame.time.delay(100)
